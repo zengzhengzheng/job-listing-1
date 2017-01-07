dfda1e5c2b515315c7a53ccc4343a7ac313462ae
zengzheng <zengzheng2009@gmail.com>
2017-01-08 03:03:58 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    is_admin    #在users里增加了is_admin 栏位，判断时让model去数据库捞数据
+  end
 end
